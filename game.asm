f1fdf772afb59db40262f5decd846325fbd23c15
chensiyu <445660334@qq.com>
2018-12-29 17:32:44 +0800

    update

--- game.asm
+++ game.asm
@@ -33,10 +33,10 @@ sl@ok:		mov ax, si
 			pop bp
 			ret 2
 
-;函数putch(byte char, byte property, byte y, byte x)
+;函数putc(byte char, byte property, byte y, byte x)
 ;功能: 将字符输出在指定位置
 ;参数: char: 字符， property: 输出属性, y: 纵坐标, x: 横坐标
-putch:		push bp
+putc:		push bp
 			mov bp, sp
 			push ds 
 			push bx
@@ -48,7 +48,7 @@ putch:		push bp
 			cmp bh, 24
 			ja pc@ok
 
-			mov ax, 20
+			mov ax, 10
 			mul bh
 			add ax, 0b800H				;计算y坐标
 			mov ds, ax
@@ -63,21 +63,65 @@ pc@ok:		pop cx
 			pop ds
 			pop bp
 			ret 4
+;函数putstr(byte[] str, byte x, byte y)
+;功能: 在指定位置输出字符串
+;参数: str: 字符串数组, x: 横坐标, y: 纵坐标 
+putstr:		push bp
+			mov bp, sp
+			push bx
+			push cx
+			push si
+			push es
+			mov si, [bp + 4]		;字符串地址
+			mov bx, [bp + 6]		;bh:Y坐标, bl: X坐标
+			push [bp + 4]
+			call strlen				;获取字符串长度
+			mov cx, ax
+			mov ax, 10				;计算Y坐标
+			mul bh
+			add ax, 0b800H
+			mov es, ax
+
+			mov ax, 2				;计算X坐标
+			mul bl
+			mov bx, ax
+ps@p:		mov al, [si]			;读取字符 输出到显示缓冲区
+			mov es:[bx], al
+			mov byte ptr es:[bx + 1], 00000111B
+			inc si
+			add bx, 2
+			loop ps@p
+
+			pop es
+			pop si
+			pop cx
+			pop bx
+			pop bp
+			ret 2
 
 main:		mov ax, 0003H
 			int 10H
+
 			mov ax, stksg
 			mov ss, ax
 			mov sp, 20
 			mov ax, data
 			mov ds, ax
-			mov ah, 12
+
+			mov ah, 15 
 			mov al, 60
 			push ax
 			mov ah, 'A'
 			mov al, 01000010B
 			push ax
-			call putch
+			call putc
+
+			mov ax, 0
+			push ax
+			mov ax, offset hello
+			push ax
+			call putstr
+
 			mov ax, 4c00H
 			int 21H
 code ends
